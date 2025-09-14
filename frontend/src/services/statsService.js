98fd244fdf07bd5caeb88980a92faecd5aeabbfd
Resham011 <reshamgithub@gmail.com>
2025-09-14 12:57:39 +0530

    Updated code

--- frontend/src/services/statsService.js
+++ frontend/src/services/statsService.js
@@ -1,3 +1,7 @@
+import axios from 'axios';
+
+const API_URL = '/api/stats/';
+
 export const getStats = async (token) => {
      const config = { headers: { Authorization: `Bearer ${token}` } };
      const response = await axios.get(API_URL, config);
